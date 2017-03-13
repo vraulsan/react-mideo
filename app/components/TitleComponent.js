00c496b6af8b0ede7847d5dee7a40fe9ca12caa5
Victor <vraulsan@gmail.com>
2017-03-13 08:01:12 -0400

    expander working

--- app/components/TitleComponent.js
+++ app/components/TitleComponent.js
@@ -7,12 +7,15 @@ function TitleComponent (props) {
     return (
       <tr onClick={props.onExpandTitle}>
         <td><img className='img-rounded img-responve' src={props.details.Poster} style={styles.posterStyle}/></td>
-        <td>{props.info.Title}
+        <td style={styles.paddingTop}>{props.info.Title}
+          <hr />
           <ul>
-            <li>{props.details.Released}</li>
-            <li>{props.details.Runtime}</li>
-            <li>{props.details.Director}</li>
-            <li>{props.details.Actors}</li>
+            <li><strong>Director:</strong> {props.details.Director}</li>
+            <li><strong>Actor:</strong>  {props.details.Actors}</li>
+            <li><strong>Released:</strong>  {props.details.Released}</li>
+            <li><strong>Runtime:</strong>  {props.details.Runtime}</li>
+            <li><strong>IMDb Rating:</strong>  {props.details.imdbRating}</li>
+            <li><strong>Awards:</strong>  {props.details.Awards}</li>
           </ul>
         </td>
         <td>{props.info.Year}</td>
